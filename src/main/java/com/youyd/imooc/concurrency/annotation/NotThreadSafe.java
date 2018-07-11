950676700576785ea2e29f823b7ccdda82da92c0
elang3000 <40287742+elang3000@users.noreply.github.com>
2018-07-11 23:45:27 +0800

    11
    111

---
+++ src/main/java/com/youyd/imooc/concurrency/annotation/NotThreadSafe.java
@@ -0,0 +1,15 @@
+package com.youyd.imooc.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//课程中用来标记线程[不]安全的类或者是写法
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+	String value() default "";
+	
+}
